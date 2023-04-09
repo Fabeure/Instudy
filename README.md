9569ec15e012a3f5ca13b43a846f0083b3f1062d
Mohamed <mohamed.hannachi@insat.ucar.tn>
2023-04-09 16:25:32 +0100

    added interface

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projet Web
+Projet Web Dev GL2/2 2022/2023
---
+++ interface.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet" />
+
+    <title>INSAT 2.0</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="assets/css/sitecomponents.css" />
+    <link rel="stylesheet" href="assets/css/owl.css" />
+    <link rel="stylesheet" href="assets/css/lightbox.css" />
+    <link rel="stylesheet" href="assets/css/img.css" />
+
+
+
+
+
+</head>
+
+<body>
+    <!--header-->
+    <header class="main-header clearfix" role="header">
+        <div class="logo">
+            <img class="imgv2" src="\assets\images\download.png" alt="INSAT 2.0" />
+        </div>
+        <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
+        <nav id="menu" class="main-nav" role="navigation">
+            <ul class="main-menu">
+                <li><a href="#section1">Home</a></li>
+                <li class="has-submenu">
+                    <a href="#section2">About Us</a>
+                    <ul class="sub-menu">
+                        <li><a href="#section2">Who we are?</a></li>
+                        <li><a href="#section3">What we do?</a></li>
+                        <li><a href="#section3">How it works?</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="#section4">Courses</a></li>
+                <li class="has-submenu"><a href="#section5">Sign in</a>
+                    <ul class="dropdown-menu" aria-labelledby="signin-btn">
+                        <form class="px-4 py-3">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" id="username" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" placeholder="Password">
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="remember">
+                                <label class="form-check-label" for="remember">Remember me</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign In</button>
+                        </form>
+                    </ul>
+
+
+
+
+                </li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </ul>
+        </nav>
+    </header>
+    <div class="dropdown-menu" aria-labelledby="signin-btn">
+        <form class="px-4 py-3">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="remember">
+                <label class="form-check-label" for="remember">Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign In</button>
+        </form>
+    </div>
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <section class="section main-banner" id="top" data-section="section1">
+        <img class="imgv3" src="\assets\images\background.jpg" alt="INSAT 2.0" />
+
+        <div class="card-img-overlay">
+            <div class="caption">
+                <h2><em>Your</em> Guide</h2>
+                <div class="main-button">
+                    <div class="scroll-to-section">
+                        <a href="#section2">Discover more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <section class="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-12">
+                    <div class="features-post">
+                        <div class="features-content">
+                            <div class="content-show">
+                                <h4><i class="fa fa-pencil"></i>All Courses</h4>
+                            </div>
+                            <div class="content-hide">
+                                <p>
+                                    Curabitur id eros vehicula, tincidunt libero eu, lobortis
+                                    mi. In mollis eros a posuere imperdiet. Donec maximus
+                                    elementum ex. Cras convallis ex rhoncus, laoreet libero eu,
+                                    vehicula libero.
+                                </p>
+                                <p class="hidden-sm">
+                                    Curabitur id eros vehicula, tincidunt libero eu, lobortis
+                                    mi. In mollis eros a posuere imperdiet.
+                                </p>
+                                <div class="scroll-to-section">
+                                    <a href="#section2">More Info.</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="features-post second-features">
+                        <div class="features-content">
+                            <div class="content-show">
+                                <h4><i class="fa fa-graduation-cap"></i>Demo2</h4>
+                            </div>
+                            <div class="content-hide">
+                                <p>
+                                    Curabitur id eros vehicula, tincidunt libero eu, lobortis
+                                    mi. In mollis eros a posuere imperdiet. Donec maximus
+                                    elementum ex. Cras convallis ex rhoncus, laoreet libero eu,
+                                    vehicula libero.
+                                </p>
+                                <p class="hidden-sm">
+                                    Curabitur id eros vehicula, tincidunt libero eu, lobortis
+                                    mi. In mollis eros a posuere imperdiet.
+                                </p>
+                                <div class="scroll-to-section">
+                                    <a href="#section3">Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <div class="features-post third-features">
+                        <div class="features-content">
+                            <div class="content-show">
+                                <h4><i class="fa fa-book"></i>Demo 3</h4>
+                            </div>
+                            <div class="content-hide">
+                                <p>
+                                    Curabitur id eros vehicula, tincidunt libero eu, lobortis
+                                    mi. In mollis eros a posuere imperdiet. Donec maximus
+                                    elementum ex. Cras convallis ex rhoncus, laoreet libero eu,
+                                    vehicula libero.
+                                </p>
+                                <p class="hidden-sm">
+                                    Curabitur id eros vehicula, tincidunt libero eu, lobortis
+                                    mi. In mollis eros a posuere imperdiet.
+                                </p>
+                                <div class="scroll-to-section">
+                                    <a href="#section4">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section coming-soon" data-section="section3">
+        <div class="col-md-5">
+            <div class="right-content">
+                <div class="top-content">
+                    <h6>Register now</h6>
+                </div>
+                <form id="contact" action="" method="get">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <fieldset>
+                                <input name="name" type="text" class="form-control" id="name" placeholder="Your Name"
+                                    required="" />
+                            </fieldset>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <input name="email" type="text" class="form-control" id="email" placeholder="Your Email"
+                                    required="" />
+                            </fieldset>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <input name="phone-number" type="text" class="form-control" id="phone-number"
+                                    placeholder="Your Phone Number" required="" />
+                            </fieldset>
+                        </div>
+                        <div class="col-md-12">
+                            <fieldset>
+                                <button type="submit" id="form-submit" class="button">
+                                    Sign in now
+                                </button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/tabs.js"></script>
+    <script src="assets/js/slick-slider.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+    $(".nav li:first").addClass("active");
+
+    var showSection = function showSection(section, isAnimate) {
+        var direction = section.replace(/#/, ""),
+            reqSection = $(".section").filter(
+                '[data-section="' + direction + '"]'
+            ),
+            reqSectionPos = reqSection.offset().top - 0;
+
+        if (isAnimate) {
+            $("body, html").animate({
+                    scrollTop: reqSectionPos,
+                },
+                800
+            );
+        } else {
+            $("body, html").scrollTop(reqSectionPos);
+        }
+    };
+
+    var checkSection = function checkSection() {
+        $(".section").each(function() {
+            var $this = $(this),
+                topEdge = $this.offset().top - 80,
+                bottomEdge = topEdge + $this.height(),
+                wScroll = $(window).scrollTop();
+            if (topEdge < wScroll && bottomEdge > wScroll) {
+                var currentId = $this.data("section"),
+                    reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+                reqLink
+                    .closest("li")
+                    .addClass("active")
+                    .siblings()
+                    .removeClass("active");
+            }
+        });
+    };
+
+    $(".main-menu, .scroll-to-section").on("click", "a", function(e) {
+        if ($(e.target).hasClass("external")) {
+            return;
+        }
+        e.preventDefault();
+        $("#menu").removeClass("active");
+        showSection($(this).attr("href"), true);
+    });
+
+    $(window).scroll(function() {
+        checkSection();
+    });
+    </script>
+
+</body>
+
+</html>
