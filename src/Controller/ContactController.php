f13f27428a9269de9699dd212ca1cf4bf5162865
Fabeure <sazaouzi@gmail.com>
2023-04-19 19:31:42 +0200

    added emailing backend to controller, needs fixing

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,33 +4,66 @@
 
 namespace App\Controller;
 
-use App\Entity\Message;
-use App\Form\MessageFormType;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use SebastianBergmann\Environment\Console;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, MailerInterface $mailer, EntityManagerInterface $entityManager): Response
     {
-        $contactMessage = new Message();
-        $form = $this->createForm(MessageFormType::class, $contactMessage);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($this->getUser()){
+            $id = $this->getUser()->getId();
+            $user = $entityManager->getRepository(User::class)->find($id);
+            $defaultData = ['message' => 'Type your message here'];
+            $form = $this->createFormBuilder($defaultData)
+                ->add('subject', TextType::class)
+                ->add('content', TextType::class)
+                ->getForm();
+            $form->handleRequest($request);
 
-            $contactMessage->setCreatedAt(new \DateTime());
-
-            $entityManager->persist($contactMessage);
-
-            return $this->redirectToRoute('contact');
+            if ($form->isSubmitted() && $form->isValid()) {
+                $email = (new Email())
+                    ->from($user->getEmail())
+                    ->to('sazaouzi@gmail.com')
+                    ->subject($form->get('subject')->getData())
+                    ->text($form->get('content')->getData());
+                $mailer->send($email);
+            }
         }
+        else{
+            $defaultData = ['message' => 'Type your message here'];
+            $form = $this->createFormBuilder($defaultData)
+                ->add('email', TextType::class)
+                ->add('subject', TextType::class)
+                ->add('content', TextType::class)
+                ->getForm();
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $email = (new Email())
+                    ->from($form->get('email')->getData())
+                    ->to('sazaouzi@gmail.com')
+                    ->subject($form->get('subject')->getData())
+                    ->text($form->get('content')->getData());
+                $mailer->send($email);
+            }
+
+        }
+
 
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
