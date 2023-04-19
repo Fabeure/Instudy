fa80f067524b74caf82816880ba5fd7fdaa42891
Fabeure <sazaouzi@gmail.com>
2023-04-19 18:32:25 +0200

    added migrations

---
+++ migrations/Version20230419143310.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230419143310 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD name VARCHAR(255) NOT NULL, ADD surname VARCHAR(255) NOT NULL, ADD number INT NOT NULL, ADD bio VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP name, DROP surname, DROP number, DROP bio');
+    }
+}
--- src/Form/ProfileSettingsFormType.php
+++ src/Form/ProfileSettingsFormType.php
@@ -2,7 +2,48 @@
 
 namespace App\Form;
 
-class ProfileSettingsFormType
-{
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
+class ProfileSettingsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('surname', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('phone', NumberType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('bio', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('email', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
 }
