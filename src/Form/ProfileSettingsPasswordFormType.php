dd8bfc20de70e522832aa6e337db10ea68d24ec4
Fabeure <sazaouzi@gmail.com>
2023-04-19 18:32:25 +0200

    added backend logic to profile settings with new fields for user (+migrations)

--- src/Form/ProfileSettingsPasswordFormType.php
+++ src/Form/ProfileSettingsPasswordFormType.php
@@ -2,7 +2,47 @@
 
 namespace App\Form;
 
-class ProfileSettingsPasswordFormType
-{
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
+class ProfileSettingsPasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('NewPlainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field form-control']],
+                'required' => true,
+                'first_options' => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Repeat New Password'],
+            ])
+            ->add('OldPlainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                    'mapped' => false,
+                'attr' => ['class' => 'form-control']
+                    ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
 }
