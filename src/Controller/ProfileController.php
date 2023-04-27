27d815d659cae63eaef4a37b73ea5f05d9536aff
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-04-27 01:18:59 +0100

    limit edit action and add UserRepository acces to ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,17 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\UserRepository;
 class ProfileController extends AbstractController
 {
 
-    #[Route('/profile/', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/profile/{username}', name: 'app_profile')]
+    public function index($username, UserRepository $userRepository): Response
     {
         //handle access control
         $this->denyAccessUnlessGranted('ROLE_USER', null, 'You must be a user to access this page ');
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
-        ]);
+
+        //if 'profile/sittings' it will be forwarded to ProfileSittingsController
+        if( strcmp($username ,'settings') == 0 ){
+            return $this->forward(controller: 'App\Controller\ProfileSettingsController::index');
+        } else {
+            $user = $userRepository->findOneBy(['username' => $username]);
+
+            //if there's no user with this username
+            if (!$user) {
+                $this->addFlash('error', $username . ' not found !');
+                return $this->render('home/index.html.twig');
+            }
+
+            else {
+                return $this->render('profile/index.html.twig', [
+                    'user' => $user,
+                ]);
+            }
+        }
     }
 }
