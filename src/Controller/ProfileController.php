4b59445913f87e81105548341d41011204fef521
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-04-27 12:49:21 +0100

    modify the  route to profile setting

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+
 class ProfileController extends AbstractController
 {
 
@@ -15,23 +16,17 @@ class ProfileController extends AbstractController
         //handle access control
         $this->denyAccessUnlessGranted('ROLE_USER', null, 'You must be a user to access this page ');
 
-        //if 'profile/sittings' it will be forwarded to ProfileSittingsController
-        if( strcmp($username ,'settings') == 0 ){
-            return $this->forward(controller: 'App\Controller\ProfileSettingsController::index');
+        $user = $userRepository->findOneBy(['username' => $username]);
+
+        //if there's no user with this username
+        if (!$user) {
+            $this->addFlash('error', $username . ' user does not exist !');
+            return $this->render('home/index.html.twig');
         } else {
-            $user = $userRepository->findOneBy(['username' => $username]);
-
-            //if there's no user with this username
-            if (!$user) {
-                $this->addFlash('error', $username . ' not found !');
-                return $this->render('home/index.html.twig');
-            }
-
-            else {
-                return $this->render('profile/index.html.twig', [
-                    'user' => $user,
-                ]);
-            }
+            return $this->render('profile/index.html.twig', [
+                'user' => $user,
+            ]);
         }
     }
+
 }
