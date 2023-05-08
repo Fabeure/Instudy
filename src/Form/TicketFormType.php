814c609aa9dc9ac0364e0988565bb1f614477502
IdrisOmit <saddi.idris@gmail.com>
2023-05-08 21:35:42 +0100

    add Ticket

--- src/Form/TicketFormType.php
+++ src/Form/TicketFormType.php
@@ -2,7 +2,34 @@
 
 namespace App\Form;
 
-class TicketFormType
-{
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
+
+class TicketFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Object', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control form-group',
+                    'placeholder' => 'Object',
+                    'required' => 'required',
+                    'maxlength' => '100',
+                ],
+            ])
+            ->add('Description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control form-group',
+                    'placeholder' => 'Description',
+                    'required' => 'required',
+                ],
+            ]);
+
+    }
 }
