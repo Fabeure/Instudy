aee7603acbf26335c2e93ae6441a9f01669ba122
Fabeure <sazaouzi@gmail.com>
2023-05-09 23:55:55 +0200

    background now randomly chosen for all pages

--- public/assets/js/Base.js
+++ public/assets/js/Base.js
@@ -19,3 +19,17 @@ function addNotif(content){
     notif = "<div className=\"notification-menu-item\"> <p>"+content+"</p> </div>"
     x.innerHTML += notif;
 }
+
+//get the section element (important for choosing animation)
+let section = document.getElementById('hero')
+
+//get different kinds of animations
+let classNames = ['slider-thumb1', 'slider-thumb2', 'slider-thumb3','slider-thumb4','slider-thumb5'];
+
+//get a random animation
+let randomIndex = Math.floor(Math.random() * classNames.length);
+let randomClass = classNames[randomIndex];
+
+
+//add animation
+section.classList.add('py-5', 'my-5', randomClass)
