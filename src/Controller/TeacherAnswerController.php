47e19cca7332d6e8d1c1b130b0c7ecbf622410d5
Fabeure <sazaouzi@gmail.com>
2023-05-09 14:57:42 +0200

    implemented backend for teach answer page

--- src/Controller/TeacherAnswerController.php
+++ src/Controller/TeacherAnswerController.php
@@ -2,25 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Homework;
+use App\Entity\Matiere;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TeacherAnswerController extends AbstractController
 {
-    #[Route('/teacher/answer/{student}', name: 'app_teacher_answer_student')]
-    public function answerByStudent(student  $student ): Response
+    #[Route('/teacher/answer/', name: 'app_teacher_answer_student')]
+    public function answerByStudent(EntityManagerInterface $entityManager): Response
     {
+        //handle access control
+        if(!$this->isGranted('ROLE_TEACHER')){
+
+            //add error flash message
+            $this->addFlash('error', 'Only teachers can access this page.');
+
+            //return to home
+            return $this->redirectToRoute('app_home');
+        }
+
+        //fetch all homeworks that were sent to me
+        $homeworks = $entityManager->getRepository(Homework::class)->findBy(['teacher' => $this->getUser()]);
+
+
         return $this->render('teacher_answer/index.html.twig', [
-            'student' => $student ,
+            'homeworks' => $homeworks
         ]);
     }
 
-    #[Route('/teacher/answer', name: 'app_teacher_answer')]
-    public function answer(): Response
-    {
-        return $this->render('teacher_answer/index.html.twig', [
-            'student' => 'student',
-        ]);
-    }
 }
