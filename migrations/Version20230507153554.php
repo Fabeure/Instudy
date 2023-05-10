3e830378bf09234c2e431e98248635b419f55ea4
IdrisOmit <saddi.idris@gmail.com>
2023-05-10 15:48:31 +0100

    pull main j-1

---
+++ migrations/Version20230507153554.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230507153554 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, id_prof_id INT NOT NULL, title VARCHAR(255) NOT NULL, pdf VARCHAR(255) NOT NULL, INDEX IDX_169E6FB9755C5E8E (id_prof_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB9755C5E8E FOREIGN KEY (id_prof_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB9755C5E8E');
+        $this->addSql('DROP TABLE course');
+    }
+}
