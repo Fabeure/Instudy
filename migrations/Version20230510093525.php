f26bafa837e467182c69931f2a7ba57c14ed65fb
NaouresBzeouich <naoures.bzeouich@insat.ucar.tn>
2023-05-10 13:39:34 +0100

    front end main v2

--- migrations/Version20230510093525.php
+++ migrations/Version20230510093525.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230509132615 extends AbstractMigration
+final class Version20230510093525 extends AbstractMigration
 {
     public function getDescription(): string
     {
